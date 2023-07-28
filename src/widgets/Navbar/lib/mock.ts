f958a6c73074de5a4a785e0debeba7ba5301b79e
Егор <etikhomirov@1tv.com>
2023-07-28 18:58:18 +0300

    mobile version fixes

--- src/widgets/Navbar/lib/mock.ts
+++ src/widgets/Navbar/lib/mock.ts
@@ -1,4 +1,6 @@
 import { RoutePath } from 'app/providers/RouteProvider/routeConfig/routeConfig';
+import { ReactComponent as Cart } from 'shared/assets/icons/24/cart.svg';
+import { ReactComponent as Catalogue } from 'shared/assets/icons/24/catalogue.svg';
 
 export const navbarLinks = [
   {
@@ -8,5 +10,16 @@ export const navbarLinks = [
   {
     path: RoutePath.cart,
     text: 'Корзина'
+  }
+];
+
+export const navbarIcons = [
+  {
+    path: RoutePath.main,
+    Icon: Catalogue
+  },
+  {
+    path: RoutePath.cart,
+    Icon: Cart
   }
 ];
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,29 +2,38 @@ import { memo } from 'react';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
 import { Title } from 'shared/ui/Title/Title';
 import { TitleSizes } from 'shared/ui/Title/types';
-import { navbarLinks } from '../lib/mock';
+import { navbarIcons, navbarLinks } from '../lib/mock';
 import cls from './Navbar.module.scss';
 
-export const Navbar = memo(() => {
-  const mappedNavbarLinks = navbarLinks.map(({ path, text }) => (
-    <AppLink key={path} to={path}>
-      {text}
-    </AppLink>
-  ));
+const mappedNavbarLinks = navbarLinks.map(({ path, text }) => (
+  <AppLink key={path} to={path}>
+    {text}
+  </AppLink>
+));
 
-  return (
-    <nav
-      className={cls.navbar}
-    >
+const mappedNavbarIcons = navbarIcons.map(({ path, Icon }) => (
+  <AppLink key={path} to={path}>
+    <Icon />
+  </AppLink>
+));
+
+export const Navbar = memo(() => (
+  <nav
+    className={cls.navbar}
+  >
+    <div className={cls.mobileFlex}>
       <Title
         className={cls.headerTitle}
         size={TitleSizes.XL}
         level={1}
         text='Интерьер.'
       />
-      <div className={cls.links}>
-        {mappedNavbarLinks}
+      <div className={cls.icons}>
+        {mappedNavbarIcons}
       </div>
-    </nav>
-  );
-});
+    </div>
+    <div className={cls.links}>
+      {mappedNavbarLinks}
+    </div>
+  </nav>
+));
